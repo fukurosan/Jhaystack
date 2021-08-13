20e701ea4b7271352fd6e02278772f6116ffcb2d
fukurosan <henrik.em.olofsson@gmail.com>
2021-08-14 01:20:58 +0200

    Implemented multi threaded full async scan.

--- src/Utility/Scan.test.ts
+++ src/Utility/Scan.test.ts
@@ -1,7 +1,8 @@
-import { FULL_SCAN } from "./Scan"
+import { FULL_SCAN, FULL_SCAN_ASYNC } from "./Scan"
 import { BY_OBJECT } from "../Extraction/ByObject"
 import { STARTS_WITH, CONTAINS } from "../Comparison/ComparisonStrategy"
 import Document from "../Model/Document"
+import IComparison from "../Model/IComparison"
 
 describe("Scan", () => {
 	const data = [
@@ -65,6 +66,39 @@ describe("Scan", () => {
 			expect(searchResult[0].value).toBe("Jimmy Fridge")
 			expect(searchResult[1].value).toBe("Jimmy Oven")
 			expect(searchResult[2].value).toBe("Jimmy Ham")
+		})
+	})
+
+	describe("Async full scan", () => {
+		//We have to define a local strategy here because of a problem in nyc/istanbul code coverage computation
+		//nyc/instabul will overwrite the native toString implementation on the function prototype, which causes problems with worker serialization
+		const strategy = (term: string, context: string) => {
+			return context.indexOf(term) > -1 ? 1 : 0
+		}
+
+		it("Returns root object on hit", async () => {
+			const comparisonStrategy = <IComparison>strategy
+			const searchString = "obj"
+			const result = await FULL_SCAN_ASYNC(processedData, searchString, comparisonStrategy)
+			expect(result.length).toBe(1)
+			expect(result[0].item.id).toBe("1")
+		})
+
+		it("Limits results", async () => {
+			const comparisonStrategy = <IComparison>strategy
+			const searchString = "min"
+			const result = await FULL_SCAN_ASYNC(processedData, searchString, comparisonStrategy, 1)
+			expect(result.length).toBe(1)
+		})
+
+		it("Handles various data types", async () => {
+			const comparisonStrategy = <IComparison>strategy
+			const searchResult = await FULL_SCAN_ASYNC(mixedDataTypes, "Jimmy", comparisonStrategy)
+			expect(searchResult.length).toBe(3)
+			const resultsByValue = searchResult.map(result => result.value)
+			expect(resultsByValue).toContain("Jimmy Fridge")
+			expect(resultsByValue).toContain("Jimmy Oven")
+			expect(resultsByValue).toContain("Jimmy Ham")
 		})
 	})
 })
--- src/Utility/Scan.ts
+++ src/Utility/Scan.ts
@@ -3,6 +3,7 @@ import Document from "../Model/Document"
 import Declaration from "../Model/Declaration"
 import IComparison from "../Model/IComparison"
 import IComparisonResult from "../Model/IComparisonResult"
+import { runManyInThread, getMaxThreadCount } from "../ThreadPlanner/ThreadPlanner"
 
 interface IComparisonMatch {
 	declaration: Declaration | null
@@ -24,7 +25,6 @@ export const FULL_SCAN = (documentArray: Document[], searchValue: any, compariso
 		return []
 	}
 	const matches: SearchResult[] = []
-	let numberOfFound = 0
 	for (let documentIndex = 0; documentIndex < documentArray.length; documentIndex++) {
 		const doc = documentArray[documentIndex]
 		const foundDeclaration = doc.declarations.reduce(
@@ -59,11 +59,116 @@ export const FULL_SCAN = (documentArray: Document[], searchValue: any, compariso
 					foundDeclaration.metaData
 				)
 			)
-			numberOfFound++
-			if (limit && numberOfFound >= limit) {
+			if (limit && matches.length >= limit) {
 				break
 			}
 		}
 	}
 	return matches
+}
+
+/**
+ * TODO:: Function ready, not yet implemented
+ * Finds all documents in the given document array with matching declarations. Always finds the best matching declaration. 
+ * This function is multi-threaded and runs off the main thread.
+ * @param {Document[]} documentArray - Array of documents to traverse
+ * @param {any} searchValue - Value to search for
+ * @param {IComparison} comparisonFunction - Function to use for value comparison
+ * @param {number} limit - Maximum number of matches
+ * @return {SearchResult[]} - A list of search results
+ */
+export const FULL_SCAN_ASYNC = async (
+	documentArray: Document[],
+	searchValue: any,
+	comparisonFunction: IComparison,
+	limit?: null | number
+): Promise<SearchResult[]> => {
+	if (limit !== undefined && limit !== null && limit < 1) {
+		return []
+	}
+	const matches: SearchResult[] = []
+	let promises = []
+	//Documents to be parsed per thread
+	const operationBatchSize = limit ? 200 : Math.round(documentArray.length / getMaxThreadCount())
+	//Amount of threads to queue before waiting
+	const threadBatchSize = getMaxThreadCount()
+	let documentOperations = []
+	for (let documentIndex = 0; documentIndex < documentArray.length; documentIndex++) {
+		const doc = documentArray[documentIndex]
+		const operations = doc.declarations.map(declaration => [searchValue, declaration.value])
+		documentOperations.push({
+			doc,
+			operations
+		})
+		if (!((documentIndex + 1) % operationBatchSize) || documentIndex === documentArray.length - 1) {
+			const operationHolder = [...documentOperations]
+			const flatOperationList = documentOperations.map(op => op.operations).reduce((acc, operation) => {
+				operation.forEach(args => acc.push(args))
+				return acc
+			}, [])
+			const batchPromise = runManyInThread(comparisonFunction, ...flatOperationList).then(results => {
+				operationHolder.forEach(operation => {
+					const documentResults = results.splice(0, operation.operations.length)
+					const searchResult = findDocumentMatch(documentResults, operation.doc)
+					if (searchResult) {
+						matches.push(searchResult)
+					}
+				})
+			})
+			promises.push(batchPromise)
+			documentOperations = []
+		}
+		if (limit && !((documentIndex + 1) % threadBatchSize)) {
+			await Promise.all(promises)
+			promises = []
+			if (matches.length >= limit) {
+				break
+			}
+		}
+	}
+	await Promise.all(promises)
+	if (limit && matches.length > limit) {
+		return matches.slice(0, limit)
+	}
+	return matches
+}
+
+/**
+ * TODO:: Function ready, not yet implemented
+ * Finds the best comparison result for a document and returns a search result object for it.
+ * @param declarationResults - Array of comparison results
+ * @param doc - Document that the declarations belong to
+ */
+const findDocumentMatch = (declarationResults: (number | IComparisonResult)[], doc: Document): SearchResult | null => {
+	const foundDeclaration = declarationResults.reduce(
+		(bestMatch: IComparisonMatch, comparisonResult: number | IComparisonResult, index: number) => {
+			const score = typeof comparisonResult === "number" && isFinite(comparisonResult) ? comparisonResult : (<IComparisonResult>comparisonResult).score
+			const weightedScore = score * doc.declarations[index].normalizedWeight
+			if (weightedScore > bestMatch.weightedScore) {
+				return {
+					declaration: doc.declarations[index],
+					score,
+					weightedScore,
+					metaData: typeof comparisonResult === "object" ? comparisonResult : null
+				}
+			}
+			return bestMatch
+		},
+		{ score: 0, weightedScore: 0, declaration: null, metaData: null }
+	)
+	if (foundDeclaration.score) {
+		return new SearchResult(
+			doc.origin,
+			doc.originIndex,
+			foundDeclaration.declaration!.path,
+			foundDeclaration.declaration!.originValue,
+			foundDeclaration.weightedScore,
+			foundDeclaration.score,
+			foundDeclaration.declaration!.weight,
+			foundDeclaration.declaration!.normalizedWeight,
+			foundDeclaration.metaData
+		)
+	} else {
+		return null
+	}
 }
